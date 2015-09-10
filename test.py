7802e830ef7caa755b55a40a81cbf3a225ebcf91
Heiko Heiko <heiko@heiko.org>
2015-09-10 14:54:15 +0200

    tests

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import random
 random.seed(12312421412)
 from bitcoin import privtopub, encode_pubkey, ecdsa_raw_sign, ecdsa_raw_recover
@@ -18,25 +19,30 @@ p2 = c_ecdsa_raw_recover(msg, vrs)
 assert p == p2
 assert encode_pubkey(p, 'bin') == pub
 
-def test_ecrecover(rounds = 100):
+
+def test_ecrecover(rounds=100):
     st = time.time()
-    rounds = 100
     for i in range(rounds):
         p = ecdsa_raw_recover(msg, vrs)
-    print 'py took', (time.time() - st)
+    elapsed = time.time() - st
+    print 'py took: %.2fsecs / %dμs per op / %d recoveries per sec' % \
+        (elapsed, elapsed / rounds * 10**6, rounds / elapsed)
 
-def test_cecrecover(rounds = 100):
+
+def test_cecrecover(rounds=100):
     st = time.time()
     for i in range(rounds):
         p = c_ecdsa_raw_recover(msg, vrs)
-    print 'c took', (time.time() - st)
+    elapsed = time.time() - st
+    print 'cy took: %.2fsecs / %dμs per op  / %d recoveries per sec' % \
+        (elapsed, elapsed / rounds * 10**6, rounds / elapsed)
+    print 'c  takes: 300μs per op / 3000 recoveries per sec'  # c wraped in go, according to gustav
 
 
 def profile():
     def test_profile():
         for i in range(100):
             p = c_ecdsa_raw_recover(msg, vrs)
-
 
     import pstats
     import cProfile
@@ -46,9 +52,9 @@ def profile():
     s.strip_dirs().sort_stats("time").print_stats()
 
 
-
 if __name__ == '__main__':
     # test_ecrecover()
     # test_cecrecover()
     # profile()
-    test_cecrecover(120 * 1000)
+    test_ecrecover(1000)
+    test_cecrecover(1000)
